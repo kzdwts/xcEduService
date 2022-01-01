0ef414cac8a27bf00167ef9f19cf350dee98dd70
kangyong <kangyong_wts@sina.com>
2022-01-01 21:45:34 +0800

    feat:新增查询

---
+++ xc-service-search/src/test/java/com/xuecheng/search/test/TestSearch.java
@@ -0,0 +1,17 @@
+package com.xuecheng.search.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 查询
+ *
+ * @author Kang Yong
+ * @date 2022/1/1
+ * @since 1.0.0
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestSearch {
+}
