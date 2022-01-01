39f30f8d2625a19a0c4c517db238889a151e5e5c
kangyong <kangyong_wts@sina.com>
2022-01-01 22:31:24 +0800

    '搜索'

--- xc-service-search/src/test/java/com/xuecheng/search/test/TestSearch.java
+++ xc-service-search/src/test/java/com/xuecheng/search/test/TestSearch.java
@@ -1,6 +1,9 @@
 package com.xuecheng.search.test;
 
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,4 +17,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class TestSearch {
+
+    @Autowired
+    private RestHighLevelClient client;
+
+    @Autowired
+    private RestClient restClient;
+
+    /**
+     * 搜索全部记录
+     *
+     * @author Kang Yong
+     * @date 2022/1/1
+     */
+    public void testSearchAll() {
+
+    }
 }
