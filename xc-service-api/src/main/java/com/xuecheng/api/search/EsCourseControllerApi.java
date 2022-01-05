612b3ef405ac77d0745473d5cef0e52fc9dfc1b6
kangyong <kangyong_wts@sina.com>
2022-01-05 23:02:57 +0800

    课程搜索

---
+++ xc-service-api/src/main/java/com/xuecheng/api/search/EsCourseControllerApi.java
@@ -0,0 +1,21 @@
+package com.xuecheng.api.search;
+
+import com.xuecheng.framework.domain.course.CoursePub;
+import com.xuecheng.framework.domain.search.CourseSearchParam;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * 课程搜索接口定义
+ *
+ * @author Kang Yong
+ * @date 2022/1/5
+ * @since 1.0.0
+ */
+@Api(value = "课程搜索", description = "课程搜索", tags = {"课程搜索"})
+public interface EsCourseControllerApi {
+
+    @ApiOperation("课程搜索")
+    QueryResponseResult<CoursePub> list(int page, int size, CourseSearchParam courseSearchParam);
+}
