581b2a8c0c930d34b16eb0dcf71cc89fc2a1dda7
kangyong <kangyong_wts@sina.com>
2022-01-15 23:31:41 +0800

    初始化媒资项目

---
+++ xc-service-manage-media/src/main/java/com/xuecheng/manage_media/ManageMediaApplication.java
@@ -0,0 +1,19 @@
+package com.xuecheng.manage_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 媒资服务启动类
+ *
+ * @author Kang Yong
+ * @date 2022/1/15
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ManageMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageMediaApplication.class, args);
+    }
+}
