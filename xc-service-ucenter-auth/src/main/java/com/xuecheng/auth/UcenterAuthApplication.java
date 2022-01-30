c28cbf6f55fe15eaf692b0687e32c1d38a2bdf8d
kangyong <kangyong_wts@sina.com>
2022-01-30 22:23:54 +0800

    feat: 鉴权工程初始化

---
+++ xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/UcenterAuthApplication.java
@@ -0,0 +1,20 @@
+package com.xuecheng.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 鉴权中心启动类
+ *
+ * @author Kang Yong
+ * @date 2022/1/30
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class UcenterAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcenterAuthApplication.class, args);
+    }
+
+}
