8aed771e8ca8befbadadfcedacad2dd3b3572b48
kangyong <kangyong_wts@sina.com>
2022-01-31 20:41:32 +0800

    feat: swagger接口放行

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/config/ResourceServerConfig.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/config/ResourceServerConfig.java
@@ -88,6 +88,17 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
      */
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
+        // 所有请求必须认证通过
+        http.authorizeRequests()
+                // 下边的路径放行
+                .antMatchers(
+                        "/v2/api-docs",
+                        "/swagger-resources/configuration/ui",
+                        "/swagger-resources",
+                        "/swagger-resources/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**"
+                ).permitAll()
+                .anyRequest().authenticated();
     }
 }
