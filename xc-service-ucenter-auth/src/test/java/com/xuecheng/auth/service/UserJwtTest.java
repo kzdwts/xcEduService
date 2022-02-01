986a5f03b810599813f6e4ca2245d005975b717b
kangyong <kangyong_wts@sina.com>
2022-02-01 20:19:55 +0800

    feat: jwt测试类

---
+++ xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/service/UserJwtTest.java
@@ -0,0 +1,19 @@
+package com.xuecheng.auth.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * jwt测试
+ *
+ * @author Kang Yong
+ * @date 2022/2/1
+ * @since 1.0.0
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserJwtTest {
+
+
+}
