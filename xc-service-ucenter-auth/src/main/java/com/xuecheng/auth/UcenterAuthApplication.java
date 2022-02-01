ca1be965b3500278d6abcaf3fd3bca3c4b253a0c
kangyong <kangyong_wts@sina.com>
2022-02-01 20:35:13 +0800

    feat: 启动类更新

--- xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/UcenterAuthApplication.java
+++ xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/UcenterAuthApplication.java
@@ -2,6 +2,9 @@ package com.xuecheng.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 鉴权中心启动类
@@ -10,7 +13,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2022/1/30
  * @since 1.0.0
  */
+
+@EnableDiscoveryClient
 @SpringBootApplication
+@EntityScan("com.xuecheng.framework.domain.auth")//扫描实体类
+@ComponentScan(basePackages = {"com.xuecheng.api"})//扫描接口
+@ComponentScan(basePackages = {"com.xuecheng.auth"})
+@ComponentScan(basePackages = {"com.xuecheng.framework"})//扫描common下的所有类
 public class UcenterAuthApplication {
 
     public static void main(String[] args) {
