af88272a5068aa47b12b9edb7e2999ce71d4d713
kangyong <kangyong_wts@sina.com>
2022-02-02 22:12:39 +0800

    feat: jwt解码测试

--- xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/service/UserJwtTest.java
+++ xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/service/UserJwtTest.java
@@ -9,6 +9,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.jwt.Jwt;
 import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.jwt.crypto.sign.RsaSigner;
+import org.springframework.security.jwt.crypto.sign.RsaVerifier;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -62,5 +63,26 @@ public class UserJwtTest {
         // 取出jwt令牌
         String token = jwt.getEncoded();
         System.out.println("token=" + token);
+    }
+
+    /**
+     * 资源服务使用公钥验证jwt的合法性，并对jwt解码
+     *
+     * @author Kang Yong
+     * @date 2022/2/2
+     */
+    @Test
+    public void testVerify() {
+        // jwt令牌
+        String token = "";
+        // 公钥
+        String publickey = "";
+        // 校验jwt
+        Jwt jwt = JwtHelper.decodeAndVerify(token, new RsaVerifier(publickey));
+        // 获取jwt原始内容
+        String claims = jwt.getClaims();
+        // jwt令牌
+        String encoded = jwt.getEncoded();
+        System.out.println(encoded);
     }
 }
