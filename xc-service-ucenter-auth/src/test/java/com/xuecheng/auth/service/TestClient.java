d9fa23916ad1968b6c2e3d3db2f98ea31021d295
kangyong <kangyong_wts@sina.com>
2022-02-03 21:43:35 +0800

    feat: 测试调用security接口

---
+++ xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/service/TestClient.java
@@ -0,0 +1,17 @@
+package com.xuecheng.auth.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 测试调用认证服务接口
+ *
+ * @author Kang Yong
+ * @date 2022/2/3
+ * @since 1.0.0
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestClient {
+}
