1d1b85342652b296aeebed857bfc1195a7247069
kangyong <kangyong_wts@sina.com>
2022-02-03 21:49:36 +0800

    feat: 测试调用security接口，今天太困了，明天继续，加油！

--- xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/service/TestClient.java
+++ xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/service/TestClient.java
@@ -1,8 +1,16 @@
 package com.xuecheng.auth.service;
 
+import com.xuecheng.framework.client.XcServiceList;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 /**
  * 测试调用认证服务接口
@@ -14,4 +22,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class TestClient {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Test
+    public void testClient() {
+        // 采用客户端复杂均衡，从eureka获取认证服务的ip和端口
+        ServiceInstance serviceInstance = loadBalancerClient.choose(XcServiceList.XC_SERVICE_UCENTER_AUTH);
+
+        URI uri = serviceInstance.getUri();
+        String authUrl = uri + "/auth/oauth/token";
+
+    }
 }
