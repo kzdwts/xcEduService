1b2560b4355ebd7c4cb2a4d8296336ac17a359ba
kangyong <kangyong_wts@sina.com>
2022-02-04 21:42:22 +0800

    feat: 测试调用security接口完善

--- xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/service/TestClient.java
+++ xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/service/TestClient.java
@@ -1,5 +1,6 @@
 package com.xuecheng.auth.service;
 
+import com.sun.jersey.core.util.Base64;
 import com.xuecheng.framework.client.XcServiceList;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.Map;
 
 /**
  * 测试调用认证服务接口
@@ -37,5 +47,42 @@ public class TestClient {
         URI uri = serviceInstance.getUri();
         String authUrl = uri + "/auth/oauth/token";
 
+        // 请求的内容分两部分
+        // 1、header信息，包括了http basic认证信息
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        String httpbasic = httpbasic("XcWebApp", "XcWebApp");
+        headers.add("Authorization", httpbasic);
+        // 2、包括grant_type、username、password
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "password");
+        body.add("username", "itcast");
+        body.add("password", "123");
+        HttpEntity<MultiValueMap<String, String>> multiValueMapHttpEntity = new HttpEntity<>(body, headers);
+
+        // 指定restTemplate当遇到400或401响应时候也不要抛出异常，也要正常返回值
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+                // 当响应的值为400或401时候也要正常响应，不要抛出异常
+                if (response.getRawStatusCode() != 400 && response.getRawStatusCode() != 401) {
+                    super.handleError(response);
+                }
+            }
+        });
+
+        // 远程调用申请令牌
+        ResponseEntity<Map> exchange = restTemplate.exchange(authUrl, HttpMethod.POST, multiValueMapHttpEntity, Map.class);
+        Map body1 = exchange.getBody();
+        System.out.println(body1);
+
+    }
+
+    private String httpbasic(String clientId, String clientSecret) {
+        // 将客户端id和客户端密码拼接，按“客户端id:客户端密码”
+        String str = clientId + ":" + clientSecret;
+        // 进行base64编码
+        byte[] encode = Base64.encode(str.getBytes());
+        return "Basic " + new String(encode);
     }
 }
