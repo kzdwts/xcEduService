f3d4a43ba961ec4728b844853c72f35e5a5b52e5
kangyong <kangyong_wts@sina.com>
2022-02-04 21:48:22 +0800

    feat: 认证服务业务接口定义

---
+++ xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/service/AuthService.java
@@ -0,0 +1,11 @@
+package com.xuecheng.auth.service;
+
+/**
+ * 认证服务业务接口定义
+ *
+ * @author Kang Yong
+ * @date 2022/2/4
+ * @since 1.0.0
+ */
+public interface AuthService {
+}
---
+++ xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/service/impl/AuthServiceImpl.java
@@ -0,0 +1,17 @@
+package com.xuecheng.auth.service.impl;
+
+import com.xuecheng.auth.service.AuthService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 认证服务业务实现层
+ *
+ * @author Kang Yong
+ * @date 2022/2/4
+ * @since 1.0.0
+ */
+@Slf4j
+@Service
+public class AuthServiceImpl implements AuthService {
+}
