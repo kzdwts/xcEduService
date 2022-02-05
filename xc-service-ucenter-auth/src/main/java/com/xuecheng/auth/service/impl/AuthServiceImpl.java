2acb85cb02464a2c3d1888087390785d0f48952c
kangyong <kangyong_wts@sina.com>
2022-02-05 20:24:47 +0800

    feat: 认证服务业务接口定义

--- xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/service/impl/AuthServiceImpl.java
+++ xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/service/impl/AuthServiceImpl.java
@@ -2,7 +2,12 @@ package com.xuecheng.auth.service.impl;
 
 import com.xuecheng.auth.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 认证服务业务实现层
@@ -14,4 +19,20 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class AuthServiceImpl implements AuthService {
+
+    @Value("${auth.tokenValiditySeconds}")
+    private Integer tokenValiditySeconds;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    // 认证方法
+
+
 }
