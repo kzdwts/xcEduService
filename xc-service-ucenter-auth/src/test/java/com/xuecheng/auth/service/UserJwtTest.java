1affb48c46104eb4e4248fad19159ea36584189f
kangyong <kangyong_wts@sina.com>
2022-02-06 21:29:31 +0800

    feat: token测试

--- xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/service/UserJwtTest.java
+++ xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/service/UserJwtTest.java
@@ -74,7 +74,7 @@ public class UserJwtTest {
     @Test
     public void testVerify() {
         // jwt令牌
-        String token = "";
+        String token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHQiOiIxIiwicm9sZXMiOiJyMDEscjAyIiwibmFtZSI6Im1ydCIsImlkIjoiMTIzIn0.a3pPI4RIGiI-4gYmC5aO_5iqsVaigKb_vSmjTxx-AjtujjBTENxqt6OI4K9ppx0IO3WEebBeOKQr6po-PnQvmMl7aNHPd9KWfKD_he_0Im88nXWU2JBX0ARc85rFiIB190r7kjFqlZrj7YkhwUsccC7PqfiyKg7Y6B7Ca_l98Dx3Zv6VJwYxVU159XlB6G2NGMIRmDbJhnxLUG0zZdOKBs0BsQobU-IRRCI2bEu5zTImimfogNsGciTCS-7CMN8kO4T_rSMwEe66vPMA-IjchsubfseIptYNVu9_QHbc8RNGf3wWrBcZOmTxaCIH-zAo02u1TQXyeeafLQQVzRTt8g";
         // 公钥
         String publickey = "";
         // 校验jwt
