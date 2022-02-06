00c4cf9059151929851b8b57cd1e21df5086a5d2
kangyong <kangyong_wts@sina.com>
2022-02-06 16:29:00 +0800

    feat: 项目启动报错问题修改

--- xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/UcenterAuthApplication.java
+++ xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/UcenterAuthApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 鉴权中心启动类
@@ -24,6 +27,11 @@ public class UcenterAuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UcenterAuthApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
     }
 
 }
