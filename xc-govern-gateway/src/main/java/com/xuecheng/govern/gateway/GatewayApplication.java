ebcd407f93608daad78054bb723f60568344f4f9
kangyong <kangyong_wts@sina.com>
2022-02-08 22:15:18 +0800

    feat: 初始化网关工程

---
+++ xc-govern-gateway/src/main/java/com/xuecheng/govern/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.xuecheng.govern.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关启动类
+ *
+ * @author Kang Yong
+ * @date 2022/2/8
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
