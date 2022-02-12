a3979fa4a61283f54965378ecef732dfd7375323
kangyong <kangyong_wts@sina.com>
2022-02-12 22:55:28 +0800

    feat：初始化订单工程

---
+++ xc-service-manage-order/src/main/java/com/xuecheng/order/ManageOrderApplication.java
@@ -0,0 +1,20 @@
+package com.xuecheng.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 订单工程启动类
+ *
+ * @author Kang Yong
+ * @date 2022/2/12
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ManageOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageOrderApplication.class, args);
+    }
+
+}
